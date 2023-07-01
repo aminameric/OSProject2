f36c29e52fd8af7a6e23a81f57cf514eb0acb98a
Amina Merić <116023819+aminameric@users.noreply.github.com>
2023-07-01 17:19:58 +0200

    Add files via upload

---
+++ memory.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void pause_for_input() {
+    printf("Press any key to continue...\n");
+    getchar();
+}
+
+int main() {
+    // Memory map an empty page
+    void* mapped_page = mmap(NULL, getpagesize(), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (mapped_page == MAP_FAILED) {
+        printf("Failed to mmap the page.\n");
+        return 1;
+    }
+
+    printf("Memory mapped page address: %p\n", mapped_page);
+
+    // Run the program for a long duration
+    while (1) {
+        // Pause for user input or sleep for a while
+        pause_for_input();
+        // sleep(10);
+
+        // Measure memory usage using `ps` command
+        system("ps -o pid,vsz,rss,cmd -p $PPID");
+
+        // Access proc file system to see memory image
+        FILE* fp = fopen("/proc/self/maps", "r");
+        if (fp != NULL) {
+            char buffer[1024];
+            while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+                printf("%s", buffer);
+            }
+            fclose(fp);
+        } else {
+            printf("Failed to access /proc/self/maps\n");
+        }
+    }
+    //part2
+    munmap(mapped_page, getpagesize());
+
+    return 0;
+}
---
+++ memory3.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <string.h>
+
+void pause_for_input() {
+    printf("Press any key to continue...\n");
+    getchar();
+}
+
+int main() {
+    // Memory map an empty page
+    void* mapped_page = mmap(NULL, getpagesize(), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (mapped_page == MAP_FAILED) {
+        printf("Failed to mmap the page.\n");
+        return 1;
+    }
+
+    printf("Memory mapped page address: %p\n", mapped_page);
+
+    // Write data into the memory-mapped page
+    const char* message = "Hello, Memory Mapping!";
+    size_t message_size = strlen(message) + 1;
+    memcpy(mapped_page, message, message_size);
+
+    // Run the program for a long duration
+    while (1) {
+        // Pause for user input or sleep for a while
+        pause_for_input();
+        // sleep(10);
+
+        // Measure memory usage using `ps` command
+        system("ps -o pid,vsz,rss,cmd -p $PPID");
+
+        // Access proc file system to see memory image
+        FILE* fp = fopen("/proc/self/maps", "r");
+        if (fp != NULL) {
+            char buffer[1024];
+            while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+                printf("%s", buffer);
+            }
+            fclose(fp);
+        } else {
+            printf("Failed to access /proc/self/maps\n");
+        }
+    }
+
+    // Unmap the memory page
+    munmap(mapped_page, getpagesize());
+
+    return 0;
+}
+
